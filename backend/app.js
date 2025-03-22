ccabcd4a1ac32e8811a988f65a92dcec7915d2d7
Gabriel Batista <gabriel.magnago14@gmail.com>
2025-03-22 19:38:12 -0300

    first commit

---
+++ backend/app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
+
+const port = process.env.PORT;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+//Routes
+const router  = require("./routes/Router.js");
+
+app.use(router);
+
+app.listen(port, () => {
+    console.log(`App rodando na porta ${port}`);
+});
